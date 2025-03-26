02955474aed593d6c7d95d21b0eb743ba3e7610f
Quynh Phan <qphan204@gmail.com>
2025-03-26 23:36:00 +0700

    First commit

---
+++ src/main/java/example/btljava2/BtlJava2.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package example.btljava2;
+
+/**
+ *
+ * @author qphan
+ */
+public class BtlJava2 {
+
+    public static void main(String[] args) {
+        MainForm frame = new MainForm();
+        frame.setVisible(true);
+    }
+}
---
+++ src/main/java/example/btljava2/DatabaseConnection.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package example.btljava2;
+
+import java.sql.*;
+import java.util.Properties;
+
+/**
+ *
+ * @author qphan
+ */
+public class DatabaseConnection {
+    public static Connection getConnection() {
+        String URL = "jdbc:mysql://localhost:3306/btl_java";
+        String USER = "root";
+        String PASSWORD = "";
+        try {
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e){
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
